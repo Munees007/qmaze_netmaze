a5964c1ecc3df8de4b03d4aaf1aa2a9e0a3d518d
Munees007 <muneesm458@gmail.com>
2025-01-07 21:19:50 +0530

    form changed to google sign in

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 const Form: React.FC<Props> = ({ onGoogleSignIn }) => {
   return (
-    <div className={`fixed top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center bg-gray-700 bg-opacity-50 z-50 
+    <div className={`fixed bg-blue-900 top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center z-50 
         `}>
-      <div className="flex flex-col items-center w-96 h-72  bg-white  justify-around rounded-lg shadow-lg">
+      <div className="flex flex-col items-center w-96 h-fit gap-2  bg-white p-5 rounded-lg shadow-lg">
         <img
           src="./netmaze.png" // Replace with your image URL
           alt="NetMaze Logo"
-          className="mb-6 w-32 h-32"
+          className="w-52 h-52"
         />
         <button
           onClick={onGoogleSignIn}
